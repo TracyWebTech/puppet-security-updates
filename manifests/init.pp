f0810895a08d185fbeaab3cfb4636988dc992979
Sergio Oliveira <seocam@seocam.com>
2016-02-18 13:58:27 -0200

    Updated to support Debian Jessie

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 
 class security_updates {
 
-  case $osfamily {
+  case $::osfamily {
     redhat: {
       cron { 'security-updates':
         command => '/usr/bin/yum update --security -y',
@@ -12,23 +12,56 @@ class security_updates {
     }
 
     debian: {
-      ensure_packages(['unattended-upgrades', 'update-notifier-common'])
 
-      file_line { 'enable unattended-upgrades':
-        path    => '/etc/apt/apt.conf.d/10periodic',
-        line    => 'APT::Periodic::Unattended-Upgrade "1";',
+      case $::operatingsystem {
+        ubuntu: {
+          ensure_packages(['update-notifier-common'])
+          $periodic_conf_path = '/etc/apt/apt.conf.d/10periodic'
+        }
+        default: {
+          $periodic_conf_path = '/etc/apt/apt.conf.d/02periodic'
+        }
+      }
+
+      ensure_packages(['unattended-upgrades'])
+
+      # Enable the update/upgrade script (0=disable)
+      file_line { 'enable apt periodic tasks':
+        path    => $periodic_conf_path,
+        line    => 'APT::Periodic::Enable "1";',
+        match   => '^APT::Periodic::Enable',
         require => Package['unattended-upgrades'],
       }
 
+      # Do "apt-get update" automatically every n-days (0=disable)
+      file_line { 'apt update':
+        path    => $periodic_conf_path,
+        line    => 'APT::Periodic::Update-Package-Lists "1";',
+        match   => '^APT::Periodic::Update-Package-Lists',
+        require => Package['unattended-upgrades'],
+      }
+
+      # Do "apt-get upgrade --download-only" every n-days (0=disable)
       file_line { 'download-upgradate-packages':
-        path    => '/etc/apt/apt.conf.d/10periodic',
+        path    => $periodic_conf_path,
         line    => 'APT::Periodic::Download-Upgradeable-Packages "1";',
         match   => '^APT::Periodic::Download-Upgradeable-Packages',
         require => Package['unattended-upgrades'],
       }
 
+      # Run the "unattended-upgrade" security upgrade script every n-days (0=disabled)
+      #   Requires the package "unattended-upgrades" and will write
+      #   a log in /var/log/unattended-upgrades
+      file_line { 'enable unattended-upgrades':
+        path    => $periodic_conf_path,
+        line    => 'APT::Periodic::Unattended-Upgrade "1";',
+        match   => '^APT::Periodic::Unattended-Upgrade',
+        require => Package['unattended-upgrades'],
+      }
+
+      # Do "apt-get autoclean" every n-days (0=disable)
       file_line { 'auto-clean':
-        path    => '/etc/apt/apt.conf.d/10periodic',
+        path    => $periodic_conf_path,
         line    => 'APT::Periodic::AutocleanInterval "7";',
         match   => '^APT::Periodic::AutocleanInterval',
         require => Package['unattended-upgrades'],
